45c520e0e8cadb33533f22ccbff5ca082c90041e
Raphael Negrisoli Batista <raphaelnegrisoli@gmail.com>
2014-07-29 18:09:05 +0000

    Format to UTF-8 encode, unix line break and space identation.
     - Ignore the performance test.

--- src/main/java/concurrent/list/SynchronizedLinkedList.java
+++ src/main/java/concurrent/list/SynchronizedLinkedList.java
@@ -7,208 +7,208 @@ import java.util.concurrent.Semaphore;
  * 
  * <p>
  * Whenever you make a change in an element:
- * <ul>
- * 	<li>Acquire the predecessor semaphore (or headSemaphore);</li>
- *  <li>Acquire the successor semaphore (or tailSemaphore);</li>
- *  <li>Make the change;</li>
- *  <li>Release the successor semaphore (or tailSemaphore);</li>
- *  <li>Release the predecessor semaphore (or headSemaphore);</li>
- *  </ul>
- *  
+ * <ol>
+ * <li>Acquire the predecessor semaphore (or headSemaphore);</li>
+ * <li>Acquire the successor semaphore (or tailSemaphore);</li>
+ * <li>Make the change;</li>
+ * <li>Release the successor semaphore (or tailSemaphore);</li>
+ * <li>Release the predecessor semaphore (or headSemaphore);</li>
+ * </ol>
+ *
  * @author Raphael Negrisoli Batista
  *
  * @param <E>
  */
 public class SynchronizedLinkedList<E> {
-	
-	private Node<E> head = null;
-	private Node<E> tail = null;
-	private Semaphore headSemaphore = new Semaphore(1, true);
-	private Semaphore tailSemaphore = new Semaphore(1, true);
-	private int size = 0;
 
-	private static class Node<E> {
-		E element;
-		Node<E> next;
-		Node<E> prev;
-		Semaphore semaphore = new Semaphore(1, true);
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private Semaphore headSemaphore = new Semaphore(1, true);
+    private Semaphore tailSemaphore = new Semaphore(1, true);
+    private int size = 0;
 
-		Node(Node<E> prev, E element, Node<E> next) {
-			this.element = element;
-			this.next = next;
-			this.prev = prev;
-		}
-	}
+    private static class Node<E> {
+        E element;
+        Node<E> next;
+        Node<E> prev;
+        Semaphore semaphore = new Semaphore(1, true);
 
-	public void insertFirst(E element) {
-		headSemaphore.acquireUninterruptibly();
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
 
-		final Node<E> oldHead = this.head;
+    public void insertFirst(E element) {
+        headSemaphore.acquireUninterruptibly();
 
-		Semaphore oldHeadSemaphore = oldHead != null? oldHead.semaphore : tailSemaphore;
-		oldHeadSemaphore.acquireUninterruptibly();
-		
-		final Node<E> newHead = new Node<>(null, element, oldHead);
-		
-		this.head = newHead;
-		
-		if (oldHead == null) {
-			this.tail = newHead;
-		} else {
-			oldHead.prev = newHead;
-		}
-		
-		size++;
-		
-		oldHeadSemaphore.release();
-		headSemaphore.release();
-	}
+        final Node<E> oldHead = this.head;
 
-	public void insertLast(E element) {
-		final Node<E> oldTail = this.tail;
-		
-		Semaphore oldTailSemaphore = oldTail != null? oldTail.semaphore : headSemaphore;
-		oldTailSemaphore.acquireUninterruptibly();
-		tailSemaphore.acquireUninterruptibly();
-		
-		final Node<E> newTail = new Node<>(oldTail, element, null);
-		
-		this.tail = newTail;
-		
-		if (oldTail == null) {
-			this.head = newTail;
-		} else {
-			oldTail.next = newTail;
-		}
-		
-		size++;
-		
-		oldTailSemaphore.release();
-		tailSemaphore.release();
-	}
-	
-	public void insertAt(E element, int index) {
-		checkPositionIndex(index);
+        Semaphore oldHeadSemaphore = oldHead != null ? oldHead.semaphore : tailSemaphore;
+        oldHeadSemaphore.acquireUninterruptibly();
 
-		if (index == size) {
-			insertLast(element);
-		} else {
-			insertBefore(element, node(index));
-		}
-	}
-	
-	public E removeFirst() {
-		return remove(head);
-	}
-	
-	public E removeLast() {
-		return remove(tail);
-	}
-	
-	public E removeAt(int index) {
-		return remove(node(index));
-	}
-	
-	public E getFirst() {
-		final Node<E> f = head;
-		return f.element;
-	}
-	
-	public E getLast() {
-		final Node<E> l = tail;
-		return l.element;
-	}
-	
-	public E get(int index) {
-		final Node<E> node = node(index);
-		return node.element;
-	}
-	
-	private void checkPositionIndex(int index) {
-		if (!isPositionIndex(index)) {
-			throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-		}	
-	}
+        final Node<E> newHead = new Node<>(null, element, oldHead);
 
-	private boolean isPositionIndex(int index) {
-		return index >= 0 && index <= size;
-	}
+        this.head = newHead;
 
-	private void insertBefore(E element, Node<E> succ) {
-		final Node<E> pred = succ.prev;
+        if (oldHead == null) {
+            this.tail = newHead;
+        } else {
+            oldHead.prev = newHead;
+        }
 
-		Semaphore predSemaphore = pred != null? pred.semaphore : headSemaphore;
-		predSemaphore.acquireUninterruptibly();
-		succ.semaphore.acquireUninterruptibly();
+        size++;
 
-		final Node<E> newNode = new Node<>(pred, element, succ);
-		
-		succ.prev = newNode;
-		if (pred == null) {
-			this.head = newNode;
-		} else {
-			pred.next = newNode;
-		}
-		
-		size++;
-		
-		predSemaphore.release();
-		succ.semaphore.release();
-	}
-	
-	private Node<E> node(int index) {
-		Node<E> node;
-		if (index < (size >> 1)) {
-			node = this.head;
+        oldHeadSemaphore.release();
+        headSemaphore.release();
+    }
 
-			for (int i = 0; i < index; i++) {
-				node = node.next;
-			}
-		} else {
-			node = this.tail;
+    public void insertLast(E element) {
+        final Node<E> oldTail = this.tail;
 
-			for (int i = size - 1; i > index; i--) {
-				node = node.prev;
-			}
-		}
+        Semaphore oldTailSemaphore = oldTail != null ? oldTail.semaphore : headSemaphore;
+        oldTailSemaphore.acquireUninterruptibly();
+        tailSemaphore.acquireUninterruptibly();
 
-		return node;
-	}
-	
-	private E remove(Node<E> node) {
-		final E element = node.element;
-		final Node<E> next = node.next;
-		final Node<E> prev = node.prev;
-		
-		Semaphore prevSemaphore = prev != null? prev.semaphore : headSemaphore;
-		Semaphore nextSemaphore = next != null? next.semaphore : tailSemaphore;
-		prevSemaphore.acquireUninterruptibly();
-		nextSemaphore.acquireUninterruptibly();
-		
-		if (prev == null) {
-			this.head = next;
-		} else {
-			prev.next = next;
-			node.prev = null;
-		}
-		
-		if (next == null) {
-			this.tail = prev;
-		} else {
-			next.prev = prev;
-			node.next = null;
-		}
-		
-		node.element = null;
-		size--;
-		
-		prevSemaphore.release();
-		nextSemaphore.release();
-		
-		return element;
-	}
-	
-	public int size() {
-		return size;
-	}
+        final Node<E> newTail = new Node<>(oldTail, element, null);
+
+        this.tail = newTail;
+
+        if (oldTail == null) {
+            this.head = newTail;
+        } else {
+            oldTail.next = newTail;
+        }
+
+        size++;
+
+        oldTailSemaphore.release();
+        tailSemaphore.release();
+    }
+
+    public void insertAt(E element, int index) {
+        checkPositionIndex(index);
+
+        if (index == size) {
+            insertLast(element);
+        } else {
+            insertBefore(element, node(index));
+        }
+    }
+
+    public E removeFirst() {
+        return remove(head);
+    }
+
+    public E removeLast() {
+        return remove(tail);
+    }
+
+    public E removeAt(int index) {
+        return remove(node(index));
+    }
+
+    public E getFirst() {
+        final Node<E> f = head;
+        return f.element;
+    }
+
+    public E getLast() {
+        final Node<E> l = tail;
+        return l.element;
+    }
+
+    public E get(int index) {
+        final Node<E> node = node(index);
+        return node.element;
+    }
+
+    private void checkPositionIndex(int index) {
+        if (!isPositionIndex(index)) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    private boolean isPositionIndex(int index) {
+        return index >= 0 && index <= size;
+    }
+
+    private void insertBefore(E element, Node<E> succ) {
+        final Node<E> pred = succ.prev;
+
+        Semaphore predSemaphore = pred != null ? pred.semaphore : headSemaphore;
+        predSemaphore.acquireUninterruptibly();
+        succ.semaphore.acquireUninterruptibly();
+
+        final Node<E> newNode = new Node<>(pred, element, succ);
+
+        succ.prev = newNode;
+        if (pred == null) {
+            this.head = newNode;
+        } else {
+            pred.next = newNode;
+        }
+
+        size++;
+
+        predSemaphore.release();
+        succ.semaphore.release();
+    }
+
+    private Node<E> node(int index) {
+        Node<E> node;
+        if (index < (size >> 1)) {
+            node = this.head;
+
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+        } else {
+            node = this.tail;
+
+            for (int i = size - 1; i > index; i--) {
+                node = node.prev;
+            }
+        }
+
+        return node;
+    }
+
+    private E remove(Node<E> node) {
+        final E element = node.element;
+        final Node<E> next = node.next;
+        final Node<E> prev = node.prev;
+
+        Semaphore prevSemaphore = prev != null ? prev.semaphore : headSemaphore;
+        Semaphore nextSemaphore = next != null ? next.semaphore : tailSemaphore;
+        prevSemaphore.acquireUninterruptibly();
+        nextSemaphore.acquireUninterruptibly();
+
+        if (prev == null) {
+            this.head = next;
+        } else {
+            prev.next = next;
+            node.prev = null;
+        }
+
+        if (next == null) {
+            this.tail = prev;
+        } else {
+            next.prev = prev;
+            node.next = null;
+        }
+
+        node.element = null;
+        size--;
+
+        prevSemaphore.release();
+        nextSemaphore.release();
+
+        return element;
+    }
+
+    public int size() {
+        return size;
+    }
 }
--- src/test/java/concurrent/list/SynchronizedLinkedListTest.java
+++ src/test/java/concurrent/list/SynchronizedLinkedListTest.java
@@ -6,90 +6,92 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SynchronizedLinkedListTest {
-	
-	@Test
-	public void testInserFisrt() throws Exception {
-		SynchronizedLinkedList<Integer> list = new SynchronizedLinkedList<>();
-		list.insertFirst(3);
-		list.insertFirst(2);
-		list.insertFirst(1);
-		
-		assertEquals(list.get(0), new Integer(1));
-		assertEquals(list.get(1), new Integer(2));
-		assertEquals(list.get(2), new Integer(3));
-	}
-	
-	@Test
-	public void testInsertFirstConcurrency() throws Exception {		
-		final SynchronizedLinkedList<Integer> list = new SynchronizedLinkedList<>();
-		Runnable inserter = new Runnable() {
-			@Override
-			public void run() {
-				for (int i = 0; i < 1000; i++) {
-					list.insertFirst(i);
-				}
-			}
-		};
-		
-		Thread i0 = new Thread(inserter);
-		Thread i1 = new Thread(inserter);
-		Thread i2 = new Thread(inserter);
-		Thread i3 = new Thread(inserter);
-		Thread i4 = new Thread(inserter);
-		Thread i5 = new Thread(inserter);
-		Thread i6 = new Thread(inserter);
-		Thread i7 = new Thread(inserter);
-		Thread i8 = new Thread(inserter);
-		Thread i9 = new Thread(inserter);
-		
-		i0.start();
-		i1.start();
-		i2.start();
-		i3.start();
-		i4.start();
-		i5.start();
-		i6.start();
-		i7.start();
-		i8.start();
-		i9.start();
-		
-		i0.join(1000);
-		i1.join(1000);
-		i2.join(1000);
-		i3.join(1000);
-		i4.join(1000);
-		i5.join(1000);
-		i6.join(1000);
-		i7.join(1000);
-		i8.join(1000);
-		i9.join(1000);
-		
-		assertEquals(10000, list.size());
-	}
 
-	@Test
-	public void testTime() throws Exception {
-		SynchronizedLinkedList<Integer> list0 = new SynchronizedLinkedList<>();
-		List<Integer> list1 = Collections.synchronizedList(new LinkedList<Integer>());
-		
-		long time0 = System.currentTimeMillis();
-		for (int i = 0; i < 1000; i++) {
-			list0.insertFirst(i);
-		}
-		time0 = System.currentTimeMillis() - time0;
-		
-		long time1 = System.currentTimeMillis();
-		for (int i = 0; i < 1000; i++) {
-			list1.add(0, i);
-		}
-		time1 = System.currentTimeMillis() - time1;
-		
-		System.out.println(time0);
-		System.out.println(time1);
-		
-		assertTrue(time0 < time1);
-	}
+    @Test
+    public void testInserFisrt() throws Exception {
+        SynchronizedLinkedList<Integer> list = new SynchronizedLinkedList<>();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(1);
+
+        assertEquals(list.get(0), new Integer(1));
+        assertEquals(list.get(1), new Integer(2));
+        assertEquals(list.get(2), new Integer(3));
+    }
+
+    @Test
+    public void testInsertFirstConcurrency() throws Exception {
+        final SynchronizedLinkedList<Integer> list = new SynchronizedLinkedList<>();
+        Runnable inserter = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    list.insertFirst(i);
+                }
+            }
+        };
+
+        Thread i0 = new Thread(inserter);
+        Thread i1 = new Thread(inserter);
+        Thread i2 = new Thread(inserter);
+        Thread i3 = new Thread(inserter);
+        Thread i4 = new Thread(inserter);
+        Thread i5 = new Thread(inserter);
+        Thread i6 = new Thread(inserter);
+        Thread i7 = new Thread(inserter);
+        Thread i8 = new Thread(inserter);
+        Thread i9 = new Thread(inserter);
+
+        i0.start();
+        i1.start();
+        i2.start();
+        i3.start();
+        i4.start();
+        i5.start();
+        i6.start();
+        i7.start();
+        i8.start();
+        i9.start();
+
+        i0.join(1000);
+        i1.join(1000);
+        i2.join(1000);
+        i3.join(1000);
+        i4.join(1000);
+        i5.join(1000);
+        i6.join(1000);
+        i7.join(1000);
+        i8.join(1000);
+        i9.join(1000);
+
+        assertEquals(10000, list.size());
+    }
+
+    @Test
+    @Ignore
+    public void testTime() throws Exception {
+        SynchronizedLinkedList<Integer> list0 = new SynchronizedLinkedList<>();
+        List<Integer> list1 = Collections.synchronizedList(new LinkedList<Integer>());
+
+        long time0 = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            list0.insertFirst(i);
+        }
+        time0 = System.currentTimeMillis() - time0;
+
+        long time1 = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            list1.add(0, i);
+        }
+        time1 = System.currentTimeMillis() - time1;
+
+        System.out.println(time0);
+        System.out.println(time1);
+
+        assertTrue(time0 < time1);
+    }
 }
